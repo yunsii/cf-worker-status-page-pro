f1342c15284e0408f11095e6d3e1e9842e0318c8
Yuns <yuns.xie@qq.com>
2024-03-22 22:08:44 +0800

    🐛 fix: no monitorHistoryData

--- src/pages/index/+data.ts
+++ src/pages/index/+data.ts
@@ -11,7 +11,12 @@ export async function data(pageContext: PageContext) {
 
   try {
     const { allMonitors, kvData } = await getCoreData(useRemoteMonitors)
-    const { lastUpdate, monitorHistoryData = {} } = kvData
+    const { lastUpdate, monitorHistoryData } = kvData
+
+    if (!monitorHistoryData) {
+      return kvData
+    }
+
     return {
       allMonitors,
       kvData: {
